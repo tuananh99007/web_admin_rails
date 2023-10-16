4ff005976aa1f2605c615a64becdf39748a78003
tuananh99007 <tuananh99007@gmail.com>
2023-10-16 09:45:20 +0700

    create database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+  # Tạo dữ liệu mẫu cho bảng "categories"
+  Category.create!(name: 'Category 1')
+  Category.create!(name: 'Category 2')
+  
+  # Tạo dữ liệu mẫu cho bảng "products"
+  Product.create!(
+    name: 'Product 1',
+    description: 'Noi dung chi tiet 1',
+    price: 19.99,
+    category_id: 1
+  )
+  
+  Product.create!(
+    name: 'Product 2',
+    description: 'Noi dung chi tiet 2',
+    price: 29.99,
+    category_id: 2
+  )

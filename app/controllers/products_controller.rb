030655c233fa2b39e2521ef8d2da905a2cb0f77b
tuananh99007 <tuananh99007@gmail.com>
2023-10-17 13:21:52 +0700

    setup controller view model table product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,64 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+
+
+  def index
+    @products = Product.all.includes(:category)
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    category_id = params[:product][:category_id].to_i # Lấy category_id từ params và chuyển thành số nguyên
+    @product.category_id = category_id
+
+    if @product.save
+      redirect_to products_path, notice: 'Product created successfully.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.valid?
+      if @product.update(product_params)
+        # Lấy danh sách category_ids từ params và chuyển thành một mảng
+        category_ids = params[:product][:category_id]
+        category_ids = [] if category_ids.blank? # Đảm bảo có thể xử lý trường hợp không có category_id
+        @product.update(category_id: category_ids.first) # Cập nhật trường category_id
+        redirect_to products_path, notice: 'Product updated successfully.'
+      else
+        flash.now[:error] = 'Error'
+        render :edit
+      end
+    else
+      flash.now[:alert] = 'Validation failed. Please check the form and try again.'
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, notice: 'Product deleted successfully.'
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, category_id: [])
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  belongs_to :category
+
+  def self.category_name
+      category = @categories.find { |cat| cat.id == category_id }
+      category.present? ? category.name : "Unknown Category"
+  end
 end

1a996b42e2d31724f23de9fdd62315b7b0668fd9
tuananh99007 <tuananh99007@gmail.com>
2023-10-19 14:15:42 +0700

    update name profile_image user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_16_025435) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_19_071439) do
   create_table "categories", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -35,6 +35,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_025435) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
+    t.string "name"
+    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

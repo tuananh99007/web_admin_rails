430d65fed8b7ae7ddb436099fdbf30e59909c680
anhnt <anhnt1@zigexn.vn>
2023-11-02 16:59:14 +0700

    update model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 255 }
   validates :price, presence: true
   belongs_to :category

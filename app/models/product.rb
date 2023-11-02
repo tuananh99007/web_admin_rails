a6b954efd56bbc0eb04c93f5aae0b83850651e4b
anhnt <anhnt1@zigexn.vn>
2023-11-02 17:05:29 +0700

    refactor code

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   belongs_to :category
 
   def self.category_name
-      category = @categories.find { |cat| cat.id == category_id }
-      category.present? ? category.name : "Unknown Category"
+    category = @categories.find { |cat| cat.id == category_id }
+    category.present? ? category.name : "Unknown Category"
   end
 end

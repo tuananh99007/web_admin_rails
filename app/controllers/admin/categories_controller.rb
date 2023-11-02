a7d30d39fbcd428be179b51e93ebae87fbeccdfd
anhnt <anhnt1@zigexn.vn>
2023-11-02 09:17:53 +0700

    fix css and category controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -15,11 +15,11 @@ class Admin::CategoriesController < Admin::AdminController
   def create
     @category = Category.new(category_params)
     if @category.save
-      redirect_to admin_categories_path, notice: "Category created successfully."
+      flash[:notice] = "Category created successfully."
     else
       flash[:alert] = "Failed to save category. Please try again."
-      redirect_to new_admin_category_path
     end
+    redirect_to new_admin_categories_path
   end
 
   def edit
@@ -27,10 +27,11 @@ class Admin::CategoriesController < Admin::AdminController
 
   def update
     if @category.update(category_params)
-      redirect_to admin_categories_path, notice: "Category updated successfully."
+      flash[:notice] = "Category updated successfully."
     else
-      render :edit
+      flash[:alert] = "Failed to update category. Please try again."
     end
+    render :edit
   end
 
   def destroy
